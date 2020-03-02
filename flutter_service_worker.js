fd515222d2515061957c7238384952bb829d1319
tansyo_tsk <ikuo.tansho@tokai-soft.co.jp>
2020-03-02 16:37:16 +0900

    New UI(Test phase)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-"/assets\AssetManifest.json": "5305d74f8e877f49d21ab03a6df1a082",
-"/assets\assets\audios\dora.mp3": "533b12c501203f7dce6e94b6de2dcb94",
-"/assets\assets\fonts\DSEG14Classic-Regular.woff": "a16ffec28180c2047ada961eb2519f26",
-"/assets\FontManifest.json": "b818e7a7c62cb64d2745a78fa7a084ba",
-"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "884bc90da95b8583299261b92376ffe2",
-"/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/assets/AssetManifest.json": "5305d74f8e877f49d21ab03a6df1a082",
+"/assets/assets/audios/dora.mp3": "533b12c501203f7dce6e94b6de2dcb94",
+"/assets/assets/audios/silent.mp3": "a8815c6b82ce58f54463779e60ba8434",
+"/assets/assets/fonts/DSEG14Classic-Regular.woff": "a16ffec28180c2047ada961eb2519f26",
+"/assets/FontManifest.json": "b818e7a7c62cb64d2745a78fa7a084ba",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/LICENSE": "926050cff025a998ee71454a31ab6d74",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "cf897d2a7f4012c9924bde4349354992",
-"/main.dart.js": "49e3c74c27e43a80104adbc1cd0f5547",
+"/main.dart.js": "e1e9af7766c914705c103a57be6758d2",
 "/manifest.json": "cddb195e4fe24096d28881c2c0caa0d4"
 };
 
