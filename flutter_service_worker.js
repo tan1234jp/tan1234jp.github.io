71a902f4929b0a328db47e3319d89982025318a1
tansyo_tsk <ikuo.tansho@tokai-soft.co.jp>
2020-03-06 17:24:16 +0900

    SHA-1: d450c70d7a757a10f402021ea238e1edad054a1b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -9,12 +9,17 @@ const RESOURCES = {
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/LICENSE": "d13717e1fa0d9a9cc9957218d771cf65",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/favicon.ico": "9c8ef56c5cc80f9b4651152efc50a1b6",
-"/icons/Icon-192.png": "b7d0a8f68fa2e3837beacdf9ac75b941",
-"/icons/Icon-512.png": "c83a0d22e0c1537db90862650e598c4d",
-"/index.html": "6a6d82c6fd16662d06e5c3b14ed80757",
+"/browserconfig.xml": "a493ba0aa0b8ec8068d786d7248bb92c",
+"/favicon-16x16.png": "d58860da10ca2847b3e92af62d52a613",
+"/favicon-32x32.png": "3f827c26a576faa5c9f86e07571e8993",
+"/icons/android-chrome-192x192.png": "46a6725cdbce619c5d2194bfdcac8c20",
+"/icons/android-chrome-512x512.png": "feb77d84ea9587b55c0036020f02d475",
+"/icons/apple-touch-icon.png": "3361e9ca37e3c0ec611b312123c287e2",
+"/icons/safari-pinned-tab.svg": "5066ed007a8096b626ad9a718d5485ba",
+"/index.html": "8e11d1aa3740bb75ae024997efa67aca",
 "/main.dart.js": "44efe44267ad00ba46a26886c19a1f3d",
-"/manifest.json": "cddb195e4fe24096d28881c2c0caa0d4"
+"/manifest.json": "4bd0e51b623aae9adcd5c081b4abbc65",
+"/mstile-150x150.png": "fdbcf3b0fb8116e9184c38af51f34576"
 };
 
 self.addEventListener('activate', function (event) {
